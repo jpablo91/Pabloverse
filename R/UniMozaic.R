5835329a38802a0158e947d1f080fc486c5e17b5
Jose Pablo Gomez <josepablogomez@campus-119-122.ucdavis.edu>
2022-11-07 10:53:30 -0800

    updated unimozaic

--- R/UniMozaic.R
+++ R/UniMozaic.R
@@ -1,13 +1,13 @@
 #' Function to get a univariate mozaic plot
 #' 
-#' @param Data A data frame with the variables
+#' @param data A data frame with the variables
 #' @param x a string with the name of the variable
 #' @param palette color palette for the plot
 #' @export
 
-UniMozaic <- function(Data, x, palette="Set1"){
-  Data %>%
+UniMozaic <- function(data, x, palette="Set1"){
+  data %>%
     count(eval(parse(text = x))) %>%
     mutate(Index = paste(`eval(parse(text = x))`, n, sep = '\n')) %>%
-    treemap(., index = "Index", vSize = 'n', type = 'index', title = paste('Distribution by', x), palette = "Set1")
+    treemap::treemap(., index = "Index", vSize = 'n', type = 'index', title = paste('Distribution by', x), palette = "Set1")
 } 
