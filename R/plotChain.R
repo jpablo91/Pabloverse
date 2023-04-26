b878f1e6ec0f4bf56d6818f481447bc97006b1d6
Jose Pablo Gomez <josepablogomez@Pablos-MacBook-Pro.local>
2023-04-26 15:34:49 -0700

    added a few new palettes and the plotChain function

---
+++ R/plotChain.R
@@ -0,0 +1,23 @@
+#' Function to plot a MCMC chain from simulated data
+#' 
+#' @param df the data frame with the simulation outcomes
+#' @param var A string with the name of the variable to plot
+#' @param col color of the line to plot
+#' @param lwd line width for the plot
+#' @export
+
+plotChain <- function(df, var, col = 'black', lwd = 0.7){
+  # df = s$Cum; var = 'TotalInfected'
+  d <- sapply(1:nrow(df), function(x){
+    df %>% 
+      slice(sample(size = x, 1:n())) %>% 
+      pull(!!(var)) %>% 
+      mean()
+  }) %>% 
+    data.frame(n = 1:nrow(df), x = .) 
+  d %>% 
+    ggplot() +
+    geom_line(aes(x = n, y = x), col = col, lwd = lwd) +
+    geom_hline(yintercept = tail(d, 1) %>% pull(x), col = 'red3', lty = 2) +
+    theme_minimal()
+}
