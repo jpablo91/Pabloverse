11bcbdd6d39bf300d0861b620d606168abdfb1bc
spablotemporal <jpgo@ucdavis.edu>
2023-07-03 11:40:30 -0700

    uploaded unemployment data

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Local Area Unemployment Statistics.
+#'
+#' Data regarding unemployment rates from 1990 to 2022
+#' 
+#' Source: https://data.ca.gov/dataset/local-area-unemployment-statistics-laus/resource/b4bc4656-7866-420f-8d87-4eda4c9996ed
+#'
+#' @format A data frame with seven variables: \code{County}, \code{Date},
+#'   \code{Year}, \code{LaborForce}, \code{Employment}, \code{Unemployment} and \code{UnemploymentRate} 
+"LAUS_CA"
