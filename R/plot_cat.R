7842cce918804bc0a918b3deb9c53bb3cc799bf3
spablotemporal <jpgo@ucdavis.edu>
2024-04-09 19:34:26 -0700

    added arguments for ordering and labs to plot_cat

--- R/plot_cat.R
+++ R/plot_cat.R
@@ -6,7 +6,7 @@
 #' @param main Main title for the figure
 #' @export
 
-plot_cat <- function(x, data, col = "red4", main = NULL, stratified = F, st){
+plot_cat <- function(x, data, col = "red4", main = NULL, stratified = F, st, n_sort = F, xlab = "", ylab = ""){
   if(is.null(main)){
     l <- select(data, {{x}}) %>% names() 
   }else{
@@ -17,10 +17,10 @@ plot_cat <- function(x, data, col = "red4", main = NULL, stratified = F, st){
       count({{x}}, {{st}}) %>%
       group_by({{st}}) %>% 
       mutate(p = round(100 * n/sum(n), 2)) %>% 
-      ggplot(aes(x = p, y = reorder({{x}}, p))) +
+      ggplot(aes(x = p, y = ifelse(n_sort, reorder({{x}}, p), {{x}}))) +
       geom_bar(aes(fill = {{st}}), stat = "identity", position = "dodge", colour = "black") +
       geom_label(aes(label = paste(p, "%"), col = {{st}}), alpha = 0.8, size = 3) +
-      labs(title = l, y = "", x = "Proportion of responses") +
+      labs(title = l, y = ylab, x = xlab) +
       # scale_fill_manual(values = c("#A08000", "#A00000")) +
       # scale_color_manual(values = c("#A08000", "#A00000")) +
       theme_bw()
@@ -28,10 +28,10 @@ plot_cat <- function(x, data, col = "red4", main = NULL, stratified = F, st){
     p <- data %>% 
       count({{x}}) %>%
       mutate(p = round(100 * n/sum(n), 2)) %>% 
-      ggplot(aes(x = p, y = reorder({{x}}, p))) +
+      ggplot(aes(x = p, y = ifelse(n_sort, reorder({{x}}, p), {{x}}))) +
       geom_bar(stat = "identity", position = "dodge", fill = col, colour = "black") +
       geom_label(aes(label = paste(p, "%")), alpha = 0.8, size = 3) +
-      labs(title = l, y = "", x = "Proportion of responses") +
+      labs(title = l, y = ylab, x = xlab) +
       theme_bw()
   }
   
